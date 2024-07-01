4feaa4fe3497cbb9d737b8b192dd366fd8efcab1
KimInteger <kjs20151240@gmail.com>
2024-07-01 10:33:31 +0900

    feat_김정수 : argument의 기준 엄격하게 설정

--- work-1.js
+++ work-1.js
@@ -1,6 +1,10 @@
 function a(b) {
-  let result = b + 10;
-  return result;
+  if(typeof(b) === 'number'){
+    if(Number.isInteger(b)){
+      let result = b + 10;
+      return result;
+    }
+  }
 }
 
-console.log(a('조우식'));
+console.log(a('1'+1));
